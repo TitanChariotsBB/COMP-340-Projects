fb014422f1167a7a9e408c776862add23056e268
Christian Abbott <cmagwoodva@gmail.com>
2024-02-15 16:50:15 -0500

    Running executables by absolute path works

--- Project1/shell.c
+++ Project1/shell.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
 #define EXIT_SUCCESS 0
 
 int shell_change_dir(char *dir_path) {
@@ -16,9 +18,9 @@ int shell_file_exists(char *file_path) {
   // use the stat() system call to check if a file exists
   // stat takes a char* for the file path and a char pointer for the output buffer
   // we don't need to check the output buffer, so pointing it to null *SHOULD* work
-  // if stat finds the file, it should return EXIT_SUCCESS (0)
-  // ignore the compiler warning
-  return (stat(file_path, NULL) == EXIT_SUCCESS);
+  // if stat finds the file, it should return true (1)
+  struct stat buffer[1000];
+  return (stat(file_path, buffer) == EXIT_SUCCESS);
 }
 
 
@@ -30,6 +32,18 @@ int shell_find_file(char *file_name, char *file_path, char file_path_size) {
 int shell_execute(char *file_path, char **argv) {
   // execute the file with the command line arguments
   // use the fork() and exec() system call 
+  pid_t pid = fork();
+  if (pid < 0) {
+    printf("Fork failed!\n");
+    return -1;
+  } else if (pid == 0) {
+    execv(file_path, argv);
+    exit(0);
+  }
+  else {
+    wait(NULL);
+    return EXIT_SUCCESS;
+  }
 }
 
 
@@ -38,7 +52,6 @@ int main (int argc, char *argv[]) {
 	char *username = getenv("USER");
 	char *hostname = getenv("HOSTNAME");
   char wd[100];
-  getcwd(wd, sizeof(wd)); // TODO: replace /home/$USERNAME with "~"
 
   // for testing purposes
 	printf("Username: %s\nHostname: %s\nCurrent Working Directory: %s\n", username, hostname, wd);
@@ -47,30 +60,32 @@ int main (int argc, char *argv[]) {
   
   //run the shell
   while (!exit) {
+    // find current directory
+    getcwd(wd, sizeof(wd)); // TODO: replace /home/$USERNAME with "~"
+
+    // get user input
     char input[50];
     printf("%s@hostname:%s$ ", username, wd);
     fgets(input, sizeof(input), stdin);
+
+    // tokenize user input
     const char *delim = " \n";
     char *token;
     char *tokens[10]; // array of token strings
     int idx = 0;
-
     token = strtok(input, delim);
     tokens[idx] = token;
-
     while (token != NULL) {
       idx++;
       token = strtok(NULL, delim);
       tokens[idx] = token;
     }
-    
     // for testing
     // int i = 0;
     // while(tokens[i] != NULL) {
     //   printf("%s\n", tokens[i]);
     //   i++;
     // }
-
 
     if (!strcmp(tokens[0], "exit") && tokens[1] == NULL) { // checks if strings are equal
       exit = 0; 
@@ -88,7 +103,14 @@ int main (int argc, char *argv[]) {
       }
       // --------------------------------------------------------
     } else {
-      printf("\"%s\" command\n", tokens[0]);
+      // absolute file path
+      if (shell_file_exists(tokens[0])) {
+        if (shell_execute(tokens[0], tokens) < 0) {
+          printf("Error: could not execute file\n");
+        }
+      } else { // $PATH or ./
+        printf("Error: could not find executable: %s\n", strerror(errno));
+      }
     }
   }
 
